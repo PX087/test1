7f149f045ec9af40258776eec391b0ad594993b4
PX_87 <luisaleela@gmail.com>
2025-06-18 23:24:46 -0300

    teste 2

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 print("Olá Brasil")
+def diga_olá():
+    print("Olá da Função")
+diga_olá()
+def a():
+    print("oi")
+def b(nome):
+    a()
+    print(nome)
+    print("tchau")
+a()
+b("juninho")
+#------------------------------------------------------------------------------------------
+def soma():
+    print(1+1)
+soma()
+def subtracao():
+    print(2568-1000)
+subtracao()
+def soma2(n1,n2):
+    print(n1+n2)
+soma2(63,8)
+soma2(800,1000)
+def subtracao2(n1,n2):
+    print(n1-n2)
+subtracao2(700,800)
+subtracao2(1000,450)
+subtracao2(900,800)
+#------------------------------------------------------------------------------------------
+nome=["abelha","girafa","porco"]
+print(nome)
+print(nome[0])
